2cce5bcc7f955d308dd8fbbe5bfc0a8f48b5248c
Osama Rabea <osrab3@gmail.com>
2025-02-10 18:25:32 +0200

    Added validation middleware for login and signup routes

--- src/api/routes/user.js
+++ src/api/routes/user.js
@@ -2,12 +2,13 @@
 const express = require('express')
 const router= express.Router();
 const authController = require('../controllers/authController')
+const {validationSignup,validationLogin} = require('../../middlewares/validation')
 
 
 router.route('/login')
-    .post(authController.login)
+    .post(validationLogin(),authController.login)
 
 router.route('/signup')
-    .post(authController.signup)
+    .post(validationSignup(),authController.signup)
 
 module.exports = router;
